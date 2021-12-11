525e8aaa0c2bf05868ed2ffdbc08fffed14ace9b
syhien <syhien@outlook.at>
2021-12-11 17:05:12 +0800

    增加延时 提升稳定性

--- main.js
+++ main.js
@@ -2,10 +2,12 @@ auto.waitFor()
 app.launchApp("买单吧")
 
 id("com.bankcomm.maidanba:id/bc_toolbar_container").waitFor()
-var bar = id("com.bankcomm.maidanba:id/bc_toolbar_container").findOnce()
 sleep(500)
+var bar = id("com.bankcomm.maidanba:id/bc_toolbar_container").findOnce()
+sleep(1000)
 if (bar != null) {
     bar.child(2).click()
+    sleep(500)
     text("年年奖").findOnce().parent().child(0).click()
     text("登录免费得机会 移动支付得机会 餐饮消费得机会").waitFor()
     sleep(500)
